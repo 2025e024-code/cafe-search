c43e422513fd50177a0f006f2a9b59b6b78c308a
2025e024-code <2025e024@hokuriku-u.ac.jp>
2026-07-17 10:43:00 +0900

    Add files via upload

--- cafe.py
+++ cafe.py
@@ -1,34 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+import streamlit as st
 import pandas as pd
-import plotly.express as px
 
+# 页面标题
+st.title("金沢カフェ検索")
 
-# In[2]:
-
-
+# 读取数据
 df = pd.read_csv("cafe.csv")
 
-
-# In[3]:
-
-
-df.head()
-
-
-# In[4]:
-
-
-df.columns
-
-
-# In[5]:
-
-
+# 修改列名
 df = df[[
     "Title",
     "crating_val",
@@ -36,12 +15,6 @@ df = df[[
     "Avg_price1",
     "Title_URL"
 ]]
-
-df.head()
-
-
-# In[6]:
-
 
 df.columns = [
     "店名",
@@ -51,56 +24,5 @@ df.columns = [
     "URL"
 ]
 
-df.head()
-
-
-# In[7]:
-
-
-df.iloc[0]
-
-
-# In[8]:
-
-
-df.info()
-
-
-# In[9]:
-
-
-df.sort_values("評価", ascending=False).head(10)
-
-
-# In[10]:
-
-
-fig = px.bar(
-    df.sort_values("評価", ascending=False).head(10),
-    x="店名",
-    y="評価",
-    title="評価が高いカフェ TOP10"
-)
-
-fig.show()
-
-
-# In[11]:
-
-
-fig = px.scatter(
-    df,
-    x="口コミ数",
-    y="評価",
-    hover_name="店名",
-    title="口コミ数と評価の関係"
-)
-
-fig.show()
-
-
-# In[ ]:
-
-
-
-
+# 显示数据
+st.dataframe(df)
