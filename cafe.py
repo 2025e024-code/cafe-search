2eedf9459950b17efc7490b752172407c75e7705
2025e024-code <2025e024@hokuriku-u.ac.jp>
2026-07-17 10:46:34 +0900

    Add files via upload

--- cafe.py
+++ cafe.py
@@ -23,6 +23,26 @@ df.columns = [
     "予算",
     "URL"
 ]
+st.sidebar.header("検索条件")
 
+min_rating = st.sidebar.slider(
+    "最低評価",
+    float(df["評価"].min()),
+    float(df["評価"].max()),
+    3.0,
+    0.1
+)
+
+min_review = st.sidebar.slider(
+    "最低口コミ数",
+    0,
+    int(df["口コミ数"].max()),
+    0
+)
+
+df = df[
+    (df["評価"] >= min_rating) &
+    (df["口コミ数"] >= min_review)
+]
 # 显示数据
 st.dataframe(df)
